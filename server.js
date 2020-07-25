416d45214748a7fd15d51266c0b45deb8e143fde
Darrick Fisher <darrick@fishmedia.agency>
2020-07-25 15:31:02 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const exphbs = require("express-handlebars");
+const methodOverride = require('method-override');
+const app = express();
+
+app.use(methodOverride('_method'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine","handlebars");
+app.use("/", routes);
+app.use(express.static('public'));
+// Listener
+app.listen(PORT,function(){
+    console.log("App now listening at localhost:" + PORT);
+});
